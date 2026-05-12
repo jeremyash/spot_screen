cdc9d23bd14c3eba818c3a00f3091669630a6a1b
jeremyash <jeremyd.ash@gmail.com>
2026-05-12 16:01:57 -0400

    adding legend box function

--- R/ui_helpers.R
+++ R/ui_helpers.R
@@ -17,3 +17,32 @@ marker_label_opts <- labelOptions(
   direction = "auto"
 )
 
+
+sfog_legend_box <- function(label, border, bg, text) {
+  div(
+    style = "
+      display:grid;
+      grid-template-columns:260px 1fr;
+      align-items:center;
+      column-gap:18px;
+      margin-bottom:10px;
+    ",
+    
+    div(
+      style = paste0(
+        "border:4px solid ", border, ";",
+        "background-color:", bg, ";",
+        "padding:8px 14px;",
+        "font-weight:bold;",
+        "text-align:center;",
+        "border-radius:4px;"
+      ),
+      label
+    ),
+    
+    div(
+      style = "font-size:16px; text-align:left;",
+      text
+    )
+  )
+}
